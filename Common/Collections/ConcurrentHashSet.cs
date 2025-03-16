00909372df8dc5d5c9cb3e0e8a667e59679d7e5b
Igor <smanigor@mail.ru>
2025-03-16 14:17:38 +0100

    Implemented FavoritesRepository; Added tests;

---
+++ Common/Collections/ConcurrentHashSet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Concurrent;
+
+namespace Common.Collections;
+
+public class ConcurrentHashSet<T> where T : notnull
+{
+    private readonly ConcurrentDictionary<T, byte> dictionary = new();
+    public IEnumerable<T> Items => dictionary.Keys;
+
+    public bool TryAdd(T item)
+    {
+        return dictionary.TryAdd(item, 0);
+    }
+
+    public void AddRange(IEnumerable<T> items)
+    {
+        foreach (var item in items)
+        {
+            TryAdd(item);
+        }
+    }
+
+    public bool ContainsKey(T item)
+    {
+        return dictionary.ContainsKey(item);
+    }
+
+    public bool TryRemove(T item)
+    {
+        return dictionary.TryRemove(item, out _);
+    }
+}
---
+++ DataAccess/DataAccess/Repositories/Abstractions/IFavoritesRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace DataAccess.Repositories.Abstractions;
+
+public interface IFavoritesRepository
+{
+    Task<IEnumerable<string>> GetAllAsync(CancellationToken token);
+    Task<bool> InsertAsync(string id, CancellationToken token);
+    Task<bool> DeleteAsync(string id, CancellationToken token);
+    Task SaveChangesAsync(CancellationToken token);
+}
